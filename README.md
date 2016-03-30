d11c2ce34c541927f9116ecf1edfb0df8df98b7b
Edmilson Junior <edmilson.junior@iinovati.com.br>
2016-03-30 15:37:47 -0300

    GMT support added

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Char functions:
 Use Examples:
 --------------------------------------------
 
-    var date = new Date("1992-11-19 22:29");
+    var date = new Date(1992,11,19,22,29"); // 1992-11-19 22:29
     console.log(date.format('d/m/Y à\\s H:i')); //Escape example
     
 > 19/11/1992 às 22:29
--- date-format.js
+++ date-format.js
@@ -1,30 +1,35 @@
-Date.prototype.format = function(pattern) {
+Date.prototype.format = function(pattern, gmt) {
 	var result = '';
+	var date = this;
+	if(gmt != undefined) {
+		var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+		date = new Date(utc + (3600000 * gmt));
+	}
 	for (var i = 0; i < pattern.length; i++) {
 		var c = pattern[i];
 		switch (c) {
 			case 'd':
-				result += ('0' + this.getDate()).slice(-2);
+				result += ('0' + date.getDate()).slice(-2);
 				break;
 			
 			case 'm':
-				result += ('0' + String(this.getMonth()+1)).slice(-2);
+				result += ('0' + String(date.getMonth()+1)).slice(-2);
 				break;
 			
 			case 'Y':
-				result += this.getFullYear();
+				result += date.getFullYear();
 				break;
 			
 			case 'H':
-				result += ('0' + this.getHours()).slice(-2);
+				result += ('0' + date.getHours()).slice(-2);
 				break;
 
 			case 'i':
-				result += ('0' + this.getMinutes()).slice(-2);
+				result += ('0' + date.getMinutes()).slice(-2);
 				break;
 
 			case 's':
-				result += ('0' + this.getSeconds()).slice(-2);
+				result += ('0' + date.getSeconds()).slice(-2);
 				break;
 
 			case '\\':
